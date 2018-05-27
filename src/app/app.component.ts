86da31ae9fdadc15a561cfb0a79c293aa77ec55f
joe <joe@joes-MacBook-Pro.local>
2018-05-26 21:02:06 -0400

    cx css

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -2,29 +2,34 @@ import {Component, OnInit} from '@angular/core';
 import {PortfolioService} from './portfolio.service';
 import {Router} from '@angular/router';
 
+import {environment} from '../environments/environment';
+
 @Component({
-  selector: 'app-root',
-  templateUrl: './app.component.html',
-  styleUrls: ['./app.component.css']
+    selector: 'app-root',
+    templateUrl: './app.component.html',
+    styleUrls: ['./app.component.css']
 })
 export class AppComponent implements OnInit {
 
 
-  constructor(private portfolioService: PortfolioService,
-              private router: Router) {
+    public cx: boolean = environment.cx;
 
 
-  }
+    constructor(private portfolioService: PortfolioService,
+                private router: Router) {
 
-  ngOnInit() {
 
-
-    const COINS = this.portfolioService.getCoins();
-
-    if (!(COINS && Array.isArray(COINS) && COINS.length > 0)) {
-
-      this.router.navigate(['edit']);
     }
-  }
+
+    ngOnInit() {
+
+
+        const COINS = this.portfolioService.getCoins();
+
+        if (!(COINS && Array.isArray(COINS) && COINS.length > 0)) {
+
+            this.router.navigate(['edit']);
+        }
+    }
 
 }
